a8b5d29926d59e931974ac29a9f5aaab1b6ae97c
dahliasan <laninaesloco@gmail.com>
2024-01-21 17:55:36 +1100

    📱 Update main element styling in page component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ReactQueryProvider } from './react-query-provider'
 export default function Home() {
   return (
     <ReactQueryProvider>
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <main className='flex min-h-screen flex-col items-center justify-between py-8 px-4'>
         <ChatForm />
       </main>
     </ReactQueryProvider>
